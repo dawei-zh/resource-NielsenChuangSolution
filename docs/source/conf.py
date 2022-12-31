1ebc319d6fc8617dbec769b0f7ba7c33944f45c6
dawei-zh <daweiz@usc.edu>
2022-12-30 23:34:49 -0800

    adjust title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,6 +11,7 @@ import sys
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
 project = 'NielsenChuang Solution'
+html_title = "NielsenChuang Solution"
 copyright = '2022, dawei-zh'
 author = 'dawei-zh'
 
